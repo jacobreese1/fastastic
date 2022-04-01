fdd40ce0dcde692571f7683341700adddffe5486
Jacob Reese <jacobreese123@gmail.com>
2022-04-01 19:05:42 -0400

    add logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import styles from "./App.module.css";
+import logo from "./design/images/logo-type.png"
 
 function App() {
   return (
     <div className={styles.App}>
       <div>
-        <p>Text</p>
+        <header>
+          <img src={logo} alt="Logo" className={styles.logo} />
+        </header>
       </div>
     </div>
   );

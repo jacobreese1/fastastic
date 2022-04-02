6f1c180fe15b576208c0bf22d8b338c6ac4944dd
Jacob Reese <jacobreese123@gmail.com>
2022-04-02 18:50:27 -0400

    create timer component and impletment countdown functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
+import Timer from "./components/Timer";
 import Header from "./ui/Header";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <Timer />
     </div>
   );
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,50 @@
 import styles from "./Timer.module.css";
+import { useState, useEffect } from "react";
 
 const Timer = () => {
-  return <div className={styles.Timer}></div>;
+  const calculateTimeLeft = () => {
+    const currentTime = new Date();
+    const endTime = Date.parse("april 3, 2022 18:00:00");
+    const diff = endTime - currentTime;
+
+    let timeLeft = {};
+
+    if (diff > 0) {
+      timeLeft = {
+        hours: Math.floor(diff / (1000 * 60 * 60)),
+        mins: Math.floor((diff / 1000 / 60) % 60),
+        secs: Math.floor((diff / 1000) % 60)
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+  
+  return (
+    <div className={styles.Timer}>
+      <div>
+        <ul className={styles.timerContainer}>
+          <li className={styles.timerNumber}>
+            {timeLeft.hours} <br /> Hours
+          </li>
+          <li className={styles.timerNumber}>
+            {timeLeft.mins} <br /> Minutes
+          </li>
+          <li className={styles.timerNumber}>
+            {timeLeft.secs} <br /> Seconds
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Timer;

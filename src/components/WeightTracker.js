233d212774e77bd1198ce9145b0946ab26a02072
Jacob Reese <jacobreese123@gmail.com>
2022-04-18 17:40:20 -0400

    output text field into console and create initial dummy array

--- src/components/WeightTracker.js
+++ src/components/WeightTracker.js
@@ -1,18 +1,34 @@
 import styles from "./WeightTracker.module.css";
 import Button from "../ui/Button";
+import { useState } from "react";
 
 const WeightTracker = () => {
+  const [weightInput, setWeightInput] = useState();
+
+  const DUMMY_INPUTS = [145, 150, 155, 160, 165];
+
+  const inputHandler = (e) => {
+    setWeightInput(e.target.value);
+  };
+
   const onClickHandler = (e) => {
     e.preventDefault();
+    console.log(weightInput);
   };
 
   return (
-    <form className={styles.tableContainer}>
-      <input className={styles.input} placeholder="Enter weight" />
-      <div className={styles.trackerBtn}>
-        <Button type="submit" label="Submit" onClick={onClickHandler} />
-      </div>
-    </form>
+    <div>
+      <form className={styles.tableContainer}>
+        <input
+          className={styles.input}
+          placeholder="Enter weight"
+          onChange={inputHandler}
+        />
+        <div className={styles.trackerBtn}>
+          <Button type="submit" label="Submit" onClick={onClickHandler} />
+        </div>
+      </form>
+    </div>
   );
 };
 

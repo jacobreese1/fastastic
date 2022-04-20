a163def9266986b60327b8d2755fdedc0b916b03
Jacob Reese <jacobreese123@gmail.com>
2022-04-20 15:26:20 -0400

    add firebase file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: " ",
+    // authDomain: "your credentials",
+    projectId: "fastastic-1f233",
+    // storageBucket: "your credentials",
+    // messagingSenderId: "your credentials",
+    // appId: "your credentials"
+};
